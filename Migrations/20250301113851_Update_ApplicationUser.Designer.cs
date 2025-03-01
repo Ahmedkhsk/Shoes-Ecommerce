e02fe9d9d8bd03d20243ceba12e030e556aca94b
Ahmedkhsk <mrahmedkhaled2004@gmail.com>
2025-03-01 13:41:34 +0200

    Update_ApplicationUser

--- Migrations/20250301113851_Update_ApplicationUser.Designer.cs
+++ Migrations/20250301113851_Update_ApplicationUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Shoes_Ecommerce.Models;
 
@@ -11,9 +12,11 @@ using Shoes_Ecommerce.Models;
 namespace Shoes_Ecommerce.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20250301113851_Update_ApplicationUser")]
+    partial class Update_ApplicationUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -3,5 +3,14 @@
     public class ApplicationUser : IdentityUser
     {
         public string? Addresse { get; set; }
+        public string vervicationCode { get; set; } = generateVervicationCode();
+        public bool MyProperty { get; set; }
+
+        static string generateVervicationCode ()
+        {
+            Random rand = new Random();
+            return rand.Next(1000, 9999).ToString();
+        }
+
     }
 }
